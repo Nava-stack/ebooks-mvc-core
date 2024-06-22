ecae0f11c51c5a2dd4dbcef59be4bcb220d0aeea
navarasan <navarasan84@gmail.com>
2024-06-22 22:17:25 +0530

    files added

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,24 @@
+﻿using EBooksPvtLtd.Data;
+using EBooksPvtLtd.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EBooksPvtLtd.Controllers
+{
+    public class BrandController : Controller
+    {
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public BrandController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Brand> brands = _dbContext.Brand.ToList();
+            return View(brands);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EBooksPvtLtd.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EBooksPvtLtd.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brand { get; set; }
+    }
+}
